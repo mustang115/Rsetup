be6c4fa26b5dacbf88fcda624b04a5562e57bdeb
mustang115 <supreetkn@gmail.com>
2017-07-14 20:32:32 -0400

    Update ARIMA.R

--- ARIMA.R
+++ ARIMA.R
@@ -1,1 +1,10 @@
+# Time Series Analysis and its Applications #Robert Shumway #David Stoffer
+#astsa
 
+
+library(astsa)
+
+#JJ quarterly earnings
+data(jj)
+plot(jj, main="J&J Quarterly Earnings per share", type="c")
+text(jj, labels=1:4, col=1:4)
