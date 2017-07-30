6e64052e94cc55a1dea7c8d6341234caafaba526
mustang115 <supreetkn@gmail.com>
2017-07-30 00:09:28 -0400

    Update GetData_quantmod_google.R

--- GetData_quantmod_google.R
+++ GetData_quantmod_google.R
@@ -1,6 +1,12 @@
 library(quantmod)
+
+#getSymbols automatically loads the variable in the environment! DISABLE autoassign!
 getSymbols.google(Symbols="SPY", from="2007-01-01", to=Sys.Date(), env = .GlobalEnv)
 getSymbols.google("AMZN", from="2007-01-01", to=Sys.Date(), env = .GlobalEnv)
+
+#This assigns the data to aa, use this!
+aa <- getSymbols("SPY",from=Sys.Date()-10, to =Sys.Date(),auto.assign = FALSE,src = "google")
+
 dataxts <- merge(SPY$Close, AMZN$Close)
 dataxts <- merge(SPY$SPY.Close, AMZN$AMZN.Close)
 colnames(dataxts) <- c("SPY","AMZN")
