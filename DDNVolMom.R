0a1f81832f7d220d6fd8f1b400ec9163d4f732d3
mustang115 <supreetkn@gmail.com>
2017-07-31 22:11:27 -0400

    Update DDNVolMom.R

--- DDNVolMom.R
+++ DDNVolMom.R
@@ -1,3 +1,4 @@
+#https://quantstrattrader.wordpress.com/2015/02/26/the-downside-of-rankings-based-strategies/
 #https://stackoverflow.com/questions/1743698/evaluate-expression-given-as-a-string
 # Evaluate regular expression as string
 
